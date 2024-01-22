1766006748bfbd8a4d923271b5bd6ff5bc4ed821
amalal22 <amalalzabi2@gmail.com>
2024-01-22 21:21:23 +0300

    done

--- models/Books.js
+++ models/Books.js
@@ -3,6 +3,6 @@ const bookSchema = new Schema({
   Title: String,
   Author: String,
   Price: { type: Number, default: 5 },
-  image: String,
+  image: { type: String, default: "assets/pexels-photo-6273274.jpeg" },
 });
 module.exports = model("book", bookSchema);
